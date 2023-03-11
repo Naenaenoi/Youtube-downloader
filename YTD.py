7781d6506565862099b73dce02f6dfe49db8ed04
Tricky Dicky <Omegabanjoes@gmail.com>
2023-03-10 17:49:11 -0800

    cool

--- YTD.py
+++ YTD.py
@@ -9,48 +9,110 @@ app = ctk.CTk()
 app.geometry("720x480")
 app.title("YTD")
 
-def Video_download():
-    try:
-        ytLink = link.get()
-        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
-        video = ytObject.streams.get_highest_resolution()
+def mp4():
+    def Video_download():
+        try:
+            ytLink = link.get()
+            ytObject = YouTube(ytLink, on_progress_callback=on_progress)
+            video = ytObject.streams.get_highest_resolution()
 
-        title.configure(text=ytObject.title)
-        video.download(output_path="Videos")
-        finishLabel.configure(text="Download Complete")
-    except:
-        finishLabel.configure(text="INVALID LINK YOU FUCK!!!!!")
-
-
-def on_progress(stream, chunk, bytes_remaining):
-    total_size = stream.filesize
-    bytes_downloaded = total_size - bytes_remaining
-    percentage_of_completion = bytes_downloaded / total_size * 100
-    per = str(int(percentage_of_completion))
-    pPercentage.configure(text=per + '%')
-    pPercentage.update()
-
-    progressBar.set(float(percentage_of_completion) / 100)
+            title.configure(text=ytObject.title)
+            video.download(output_path="Videos")
+            finishLabel.configure(text="Download Complete")
+        except:
+            finishLabel.configure(text="INVALID LINK YOU FUCK!!!!!")
 
 
-title = ctk.CTkLabel(app, text = "Input link")
-title.pack(padx=10, pady=10)
+    def on_progress(stream, chunk, bytes_remaining):
+        total_size = stream.filesize
+        bytes_downloaded = total_size - bytes_remaining
+        percentage_of_completion = bytes_downloaded / total_size * 100
+        per = str(int(percentage_of_completion))
+        pPercentage.configure(text=per + '%')
+        pPercentage.update()
 
-url_var = tkinter.StringVar()
-link = ctk.CTkEntry(app, width=350, height=40, textvariable = url_var)
-link.pack()
+        progressBar.set(float(percentage_of_completion) / 100)
 
-finishLabel = ctk.CTkLabel(app, text="")
-finishLabel.pack()
 
-pPercentage = ctk.CTkLabel(app, text="0%")
-pPercentage.pack()
+    title = ctk.CTkLabel(app, text = "Input link")
+    title.pack(padx=10, pady=10)
 
-progressBar = ctk.CTkProgressBar(app, width=400)
-progressBar.set(0)
-progressBar.pack(padx=10, pady=10)
+    url_var = tkinter.StringVar()
+    link = ctk.CTkEntry(app, width=350, height=40, textvariable = url_var)
+    link.pack()
 
-download = ctk.CTkButton(app, text="Download", command= Video_download)
-download.pack(padx=10, pady=10)
+    finishLabel = ctk.CTkLabel(app, text="")
+    finishLabel.pack()
+
+    pPercentage = ctk.CTkLabel(app, text="0%")
+    pPercentage.pack()
+
+    progressBar = ctk.CTkProgressBar(app, width=400)
+    progressBar.set(0)
+    progressBar.pack(padx=10, pady=10)
+
+    download = ctk.CTkButton(app, text="Download", command= Video_download)
+    download.pack(padx=10, pady=10)
+
+    app.mainloop()
+
+def mp3():
+    def Music_download():
+        try:
+            ytLink = link.get()
+            ytObject = YouTube(ytLink, on_progress_callback=on_progress)
+            audio = ytObject.streams.get_audio_only()
+
+            title.configure(text=ytObject.title)
+            audio.download(output_path="Audio")
+            finishLabel.configure(text="Download Complete")
+        except:
+            finishLabel.configure(text="INVALID LINK YOU FUCK!!!!!")
+
+
+    def on_progress(stream, chunk, bytes_remaining):
+        total_size = stream.filesize
+        bytes_downloaded = total_size - bytes_remaining
+        percentage_of_completion = bytes_downloaded / total_size * 100
+        per = str(int(percentage_of_completion))
+        pPercentage.configure(text=per + '%')
+        pPercentage.update()
+
+        progressBar.set(float(percentage_of_completion) / 100)
+
+
+    title = ctk.CTkLabel(app, text = "Input link")
+    title.pack(padx=10, pady=10)
+
+    url_var = tkinter.StringVar()
+    link = ctk.CTkEntry(app, width=350, height=40, textvariable = url_var)
+    link.pack()
+
+    finishLabel = ctk.CTkLabel(app, text="")
+    finishLabel.pack()
+
+    pPercentage = ctk.CTkLabel(app, text="0%")
+    pPercentage.pack()
+
+    progressBar = ctk.CTkProgressBar(app, width=400)
+    progressBar.set(0)
+    progressBar.pack(padx=10, pady=10)
+
+    download = ctk.CTkButton(app, text="Download", command= Music_download)
+    download.pack(padx=10, pady=10)
+
+    app.mainloop()
+
+options_var = ctk.StringVar(value = "MP4")
+
+def options_callback(choice):
+    if choice == 'MP4':
+        mp4()
+    elif choice == 'MP3':
+        mp3()
+
+options = ctk.CTkOptionMenu(app, values = ["MP4", "MP3"], command = options_callback, variable = options_var)
+options.pack(padx = 20, pady = 10)
+
 
 app.mainloop()
